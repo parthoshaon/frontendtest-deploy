e5e2e26b98d60b728f966dae99827e2addbf2584
Udoka <udochukwukaonyela@gmail.com>
2022-07-09 12:00:59 +0100

    fixed counter logic

---
+++ components/Status.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Status = ({icon, text, details}) => {
+  return (
+      <div>
+          <span></span>   
+          <h1 className="text-3xl ">Awesome</h1>
+      </div>
+  )
+}
+
+export default Status;
