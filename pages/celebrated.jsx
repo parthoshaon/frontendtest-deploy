44e99b03cf5fe67241f1b0f2c081f5adac18ab63
Udoka <udochukwukaonyela@gmail.com>
2022-07-09 22:20:16 +0100

    fixed typo

--- pages/celebrated.jsx
+++ pages/celebrated.jsx
@@ -11,13 +11,13 @@ const celebrated = () => {
     <>
     <Navbar  loggedIn={state}/>
       <div className="flex flex-col items-center content mt-12">
-      <h1 className="text-center lg:text-3xl text-xl font-medium text-[#0c0114]">Celebated Techies</h1>
+      <h1 className="text-center lg:text-3xl text-xl font-medium text-[#0c0114]">Celebrated Techies</h1>
         <p className="text-center lg:text-xl text-lg font-medium text-[#808080]">A look into some of our past winners</p>
           <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-8 lg:p-12 md:p-8 p-6">
           {data.map((data)=>(
             <span className="flex items-center flex-col gap-6 p-6 shadow-xl hover:shadow-2xl group transition-all duration-500 ease-in cursor-pointer">
               <span className="lg:w-64 lg:h-64 w-24 h-24 bg-[#D1D1D6]"></span>
-              <span clasName="text-lg font-semibold">{data.name}</span>
+              <span className="text-lg font-semibold">{data.name}</span>
               <span className="text-[#3E0563] font-semibold text-base">{data.role}</span>
   </span>
           ))}
