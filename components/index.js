9bb293c838e6c2f6d2d728c51a625e130efae22b
Udoka <udochukwukaonyela@gmail.com>
2022-07-09 12:03:59 +0100

    exported statusz

--- components/index.js
+++ components/index.js
@@ -5,3 +5,4 @@ export { default as Features} from './Features';
 export { default as ConnectedAwards} from './ConnectedAwards';
 export { default as CTA} from './CTA';
 export { default as Footer} from './Footer';
+export { default as Status } from './Status';
