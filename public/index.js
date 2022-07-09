545eeabe76bfd304f245e5f38f9ada6ac20c00a0
Udoka <udochukwukaonyela@gmail.com>
2022-07-09 19:05:51 +0100

    exported icons

--- public/index.js
+++ public/index.js
@@ -3,4 +3,6 @@ import thumbs from './thumbs-up.svg';
 import close from './close.svg';
 import instagram from './instagram.svg';
 import linkedln from './linkedln.svg';
-export { rocket, thumbs, close, instagram, linkedln };
+import facebook from './facebook.svg';
+import twitter from './twitter.svg';
+export { rocket, thumbs, close, instagram, linkedln, facebook, twitter };
