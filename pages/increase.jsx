59d58b942eb7b693eb09e0592b6672cf9d49d4a2
Udoka <udochukwukaonyela@gmail.com>
2022-07-09 12:06:42 +0100

    fixed bug

--- pages/increase.jsx
+++ pages/increase.jsx
@@ -41,7 +41,7 @@ const voting = () => {
                     <Link href="/voting"><span  className="hover:text-white mx-auto bg-white px-6 py-4 rounded hover:bg-[#0c0114] text-[#0c0114] cursor-pointer transition-all duration-500 w-fit ease-in font-bold">Buy</span></Link>
                 </span>
             </div>
-            <div className="flex flex-col gap-36 bg-[#1F0332] text-white items-center text-center w-full lg:p-12 gap-10 rounded p-6">
+            <div className="flex flex-col gap-36 bg-[#1F0332] text-white items-center text-center w-full lg:p-12 rounded p-6">
                 <span className="flex flex-col text-center"><h1 className="lg:text-5xl text-3xl font-bold">100</h1><p className="lg:text-lg text-base">Votes</p></span>
                 <span className="flex flex-col text-center gap-6">
                     <h1 className="lg:text-5xl text-3xl font-medium">$10</h1>
