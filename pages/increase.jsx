e3b5c94003f7000508ac4c6f6ae3cff91e5a1e26
Udoka <udochukwukaonyela@gmail.com>
2022-07-09 13:24:43 +0100

    mobile responsiveness

--- pages/increase.jsx
+++ pages/increase.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Link from 'next/link';
-import { Navbar, Status } from '../components';
+import { Navbar } from '../components';
 import { rocket, close } from "../public";
 import Image from "next/image";
 const data = ["Exceptional Data Analyst", "Promising Data Analyst","Exceptional Data Scientist","Promising Data Scientist","Exceptional Visual Designer","Promising Visual Designer","Exceptional Devops Expert","Exceptional Data Analyst","Exceptional Data Analyst","Promising Data Analyst", "Exceptional Data Scientist","Promising Data Scientist","Exceptional Visual Designer","Promising Visual Designer","Exceptional DevOps Expert","Exceptional Data Analyst"]
@@ -21,7 +21,7 @@ const voting = () => {
       <Navbar loggedIn={state} />
       <div className={`${popUp ? `flex transition-[display] duration-500 ease-in` : `hidden transition-[display] duration-500 ease-out`} flex-col items-center`}>
           <div className="inset-0 bg-black/70 w-full h-full fixed z-30"></div>
-        <div onBlur={() => setPopUp(false)} tabIndex={90}  className="fixed z-40 bg-white w-2/5 h-auto p-10 flex flex-col items-center rounded shadow-sm gap-8">
+        <div onBlur={() => setPopUp(false)} tabIndex={90}  className="fixed z-40 bg-white lg:w-2/5 w-4/5 h-auto p-10 flex flex-col items-center rounded shadow-sm gap-8">
             <div className="float-right ml-auto cursor-pointer"
               onClick={() => setPopUp(false)}
             ><Image src={close} width="24px" height="24px" /> </div>
@@ -31,7 +31,7 @@ const voting = () => {
             <p>You have successfully Increased your voting power.</p>
             <p>Go and make your favorite candidate win!</p>
             </span>
-            <span className="p-3 bg-[#3E0563] text-white font-bold rounded px-8 tracking-wider"><Link href="/voting">Return to voting page</Link></span>
+            <span className="lg:p-3 p-2 bg-[#3E0563] text-white text-sm font-bold rounded lg:px-8 px-4 tracking-wider"><Link href="/voting">Return to voting page</Link></span>
           </div>
         </div>
       
