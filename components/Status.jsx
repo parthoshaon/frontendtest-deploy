0e05ebf254af4b822fa01e449526685975908120
Udoka <udochukwukaonyela@gmail.com>
2022-07-09 12:06:46 +0100

    styling

--- components/Status.jsx
+++ components/Status.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Status = ({icon, text, details}) => {
   return (
-      <div>
+      <div className="absolute inset-1/2 z-10">
           <span></span>   
           <h1 className="text-3xl text-[#1F0231] font-bold">Awesome</h1>
       </div>
