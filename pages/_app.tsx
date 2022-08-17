602f49397fc7a3c6716d6ea65597117a3a90d49c
Udoka <udochukwukaonyela@gmail.com>
2022-08-17 07:59:42 +0100

    added meta data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <>
+   <Head>
+     <meta name="author" content="Onyela Udochukwuka" />
+        <meta name="description" content="A site for the connected awards" />
+              <meta name="keywords" content="Connected awards, Techie, Tech Africa, West Africa, Tech personality" />
+                <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+                  <meta property="og:image:src" content="/brief.ico" />
+        <meta property="og:image" content="/rainbow.ico" />
+                      <meta property="og:image:width" content="1200" />
+                        <meta property="og:image:height" content="600" />
+                          <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta property="og:description" content="An URL Shortener Built To simplify Things." />
+                            <meta property="og:title" content="Brief" />
+    <title>The Connected Awards</title>
+        <link rel="shortcut icon" href="/rainbow.ico" />
+    </Head>
+  <Component {...pageProps} />
+  </>)
 }
 
 export default MyApp
