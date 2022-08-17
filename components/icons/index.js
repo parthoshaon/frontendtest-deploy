cba87dcd7047f7a1096b469dabb08d59c716b328
Udoka <udochukwukaonyela@gmail.com>
2022-08-17 07:37:46 +0100

    created icons

--- components/icons/index.js
+++ components/icons/index.js
@@ -2,3 +2,5 @@ export { default as Medal } from "./Medal";
 export { default as Star } from "./Star";
 export { default as Stars } from "./Stars";
 export { default as Africa } from "./Africa";
+export { default as Google } from "./Google";
+export { default as LinkedIn } from "./LinkedIn";
