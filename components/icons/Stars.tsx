9bb7f60b73da3068b64db400e733dd8c6365f0cf
Udoka <udochukwukaonyela@gmail.com>
2022-08-18 18:45:28 +0100

    added prop types

--- components/icons/Stars.tsx
+++ components/icons/Stars.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react'
 
-const Stars:FC = (props) => {
+const Stars:FC<{className: string}> = (props) => {
   return (
       <svg {...props} width="176" height="80" viewBox="0 0 176 80" fill="none" xmlns="http://www.w3.org/2000/svg">
           <g clipPath="url(#clip0_1_11209)">
