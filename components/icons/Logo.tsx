fe86d70f1bd2b701171a5fa0dacc91dfcb8edeca
Udoka <udochukwukaonyela@gmail.com>
2022-08-18 19:37:21 +0100

    added prop types

--- components/icons/Logo.tsx
+++ components/icons/Logo.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 import React, { FC } from 'react'
 import { logo } from "../../public"
 
-const Logo:FC = (props) => {
+const Logo: FC<{ className: string}> = (props) => {
   return (
    <span {...props}  ><Image src={logo} layout="responsive" /></span>
   )
