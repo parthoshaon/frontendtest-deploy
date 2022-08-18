dfad5a5dc6a542a5e5efe5016b40d72e51840661
Udoka <udochukwukaonyela@gmail.com>
2022-08-18 22:52:36 +0100

    fixes

--- pages/voting/[slug].tsx
+++ pages/voting/[slug].tsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { NextRouter, useRouter } from 'next/router';
 import { Navbar } from '../../components';
-import { ParsedUrlQuery } from "querystring";
+import { rocket, close } from "../../public";
+import Image from "next/image";
 const vote = () =>{
     const [data, setData] = useState<string[]>(["Adewale Akinola", "Adewale Akinola","Adewale Akinola","Adewale Akinola","Adewale Akinola","Adewale Akinola","Adewale Akinola","Adewale Akinola","Adewale Akinola", "Adewale Akinola","Adewale Akinola","Adewale Akinola"]);
+    const [popUp, setPopUp] = useState<boolean>(false);
     const router:NextRouter = useRouter();
     const { slug } = router.query;
     const [state, setState] = useState(false);
@@ -13,12 +15,33 @@ const vote = () =>{
       var string  = arg?.replace('-',' ');
       return arg?.indexOf('-') > -1 ? parser(string) : string;
     }
-    useEffect(() => {
+  useEffect(() => {
       return !!window.localStorage.getItem('name') ? setState(true) : setState(false);
   }, [])
+  useEffect(()=>{
+    if (vote === 0) {
+      setPopUp(true);
+    }
+  },[vote])
+  
     return (
       <>
       <Navbar  loggedIn={state}/>
+        <div className={`${popUp ? `flex transition-[display] duration-500 ease-in` : `hidden transition-[display] duration-500 ease-out`} flex-col items-center`}>
+          <div className="inset-0 bg-black/70 w-full h-full fixed z-30"></div>
+          <div onBlur={() => setPopUp(false)} tabIndex={90} className="fixed z-40 bg-white lg:w-2/5 w-4/5 h-auto p-10 flex flex-col items-center rounded shadow-sm gap-8">
+            <div className="float-right ml-auto cursor-pointer"
+              onClick={() => setPopUp(false)}
+            ><Image src={close} width="24px" height="24px" /> </div>
+            <Image src={rocket} width="96px" height="96px" />
+            <h1 className="text-3xl text-[#3E0563] tracking-wider font-bold">Awesome!</h1>
+            <span className="text-center">
+              <p>You have successfully Increased your voting power.</p>
+              <p>Go and make your favorite candidate win!</p>
+            </span>
+            <span className="lg:p-3 p-2 bg-[#3E0563] text-white text-sm font-bold rounded lg:px-8 px-4 tracking-wider"><Link href="/voting">Return to voting page</Link></span>
+          </div>
+        </div>
         <div className="flex flex-col items-center content mt-12">
             <h1 className="text-center lg:text-3xl text-xl font-medium w-4/5 text-[#0c0114] capitalize">{parser(typeof slug === "string" ? slug : "")}</h1>
             <div className="lg:w-1/2 w-4/5 flex items-center mx-auto mt-8 gap-6">
@@ -31,7 +54,8 @@ const vote = () =>{
   
                   <span className="p-4 text-[#EEE8F2] hover:bg-[#EEE8F2] bg-[#3E0563] hover:text-[#3E0563] transition-all duration-500 ease-in font-semibold rounded cursor-pointer">Search</span>
             </div>
-            <div className="text-[#0c0114]">You have <span className="text-3xl text-[#3E0563]">{vote}</span> votes left</div>
+          <div className="text-[#0c0114]">You have <span className="text-3xl text-[#3E0563]">{vote}</span> votes left</div>
+          
             <div className="text-[#0c0114]">Increase you voting power <Link href="/increase" ><span className="text-lg text-[#3E0563] cursor-pointer">Here</span></Link> </div>
             <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-8 lg:p-12 md:p-8 p-6">
             {data.map((data)=>{
