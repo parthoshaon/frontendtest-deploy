63b0af8c10d2722cb5929a8c5603ebed5311e6c6
Udoka <udochukwukaonyela@gmail.com>
2022-08-18 19:36:07 +0100

    exported Logo component

--- components/icons/index.js
+++ components/icons/index.js
@@ -7,3 +7,4 @@ export { default as LinkedIn } from "./LinkedIn";
 export { default as Facebook } from "./Facebook";
 export { default as Github } from "./Github";
 export { default as Twitter } from "./Twitter";
+export { default as Logo } from "./Logo";
