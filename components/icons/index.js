b50af8879deeb5d28f133d7e423ed613744d136d
Udoka <udochukwukaonyela@gmail.com>
2022-08-18 19:09:40 +0100

    exported icon components

--- components/icons/index.js
+++ components/icons/index.js
@@ -5,3 +5,5 @@ export { default as Africa } from "./Africa";
 export { default as Google } from "./Google";
 export { default as LinkedIn } from "./LinkedIn";
 export { default as Facebook } from "./Facebook";
+export { default as Github } from "./Github";
+export { default as Twitter } from "./Twitter";
