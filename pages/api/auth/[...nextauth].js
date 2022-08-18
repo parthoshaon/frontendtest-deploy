362ad7e9599ecb25d9b33a78aafb7bb7d742b332
Udoka <udochukwukaonyela@gmail.com>
2022-08-18 22:23:10 +0100

    added providers

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,8 +2,11 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import TwitterProvider from 'next-auth/providers/twitter';
 import GoogleProvider from 'next-auth/providers/google';
+import LinkedInProvider from 'next-auth/providers/linkedin';
+import FacebookProvider from 'next-auth/providers/facebook';
 import EmailProvider from "next-auth/providers/email";
 import clientPromise from "./lib/mongodb";
+require("dotenv").config();
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 export default NextAuth({
     providers: [
@@ -27,17 +30,17 @@ export default NextAuth({
       clientId: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET
     }),
-    EmailProvider({
-    server: {
-      host: process.env.EMAIL_SERVER_HOST,
-      port: process.env.EMAIL_SERVER_PORT,
-      auth: {
-        user: process.env.EMAIL_SERVER_USER,
-        pass: process.env.EMAIL_SERVER_PASSWORD
-      }
-    },
-    from: process.env.EMAIL_FROM
-  }),
+  //   EmailProvider({
+  //   server: {
+  //     host: process.env.EMAIL_SERVER_HOST,
+  //     port: process.env.EMAIL_SERVER_PORT,
+  //     auth: {
+  //       user: process.env.EMAIL_SERVER_USER,
+  //       pass: process.env.EMAIL_SERVER_PASSWORD
+  //     }
+  //   },
+  //   from: process.env.EMAIL_FROM
+  // }),
     ],
     pages: {
         signIn: '/auth/signIn'
