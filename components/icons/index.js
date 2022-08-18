cd78487dcdba9f31c99c28581075010e1e9b46b5
Udoka <udochukwukaonyela@gmail.com>
2022-08-18 18:51:24 +0100

    created facebook icons

--- components/icons/index.js
+++ components/icons/index.js
@@ -4,3 +4,4 @@ export { default as Stars } from "./Stars";
 export { default as Africa } from "./Africa";
 export { default as Google } from "./Google";
 export { default as LinkedIn } from "./LinkedIn";
+export { default as Facebook } from "./Facebook";
