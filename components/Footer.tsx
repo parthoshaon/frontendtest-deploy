107f3588a6d708d4b7f716064ada8b3235efe865
Udoka <udochukwukaonyela@gmail.com>
2022-08-20 17:02:31 +0100

    added components

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,37 +3,30 @@ import Image from 'next/image';
 import Link from 'next/link';
 import image from '../public/connectedAwards.png';
 import { Props } from "../typescript";
+import { FootLink } from './util';
 const about = ["The Connected Magazine" ,"Past Edition","Celebrated Techies"];
 const legal = ["Terms Of Service" ,"Privacy Policy"];
 const social = ["Linkedin"];
-const contact = ["+23470572199571","Chat On Whatsapp"];
+const contact = ["+23470572199571", "Chat On Whatsapp"];
 const Footer:FC<Props> = () => {
   return (
     <div className="bg-[#1F0231] text-white flex flex-col items-center p-12 gap-20 w-full text-xs lg:text-base">
         <div className="grid  gap-4 lg:grid-cols-4 md:grid-cols-3 grid-cols-2 w-full">
         <div className="flex flex-col gap-4">
             <Link href="/"><span className="text-[#FA9706]">About</span></Link>
-            {about.map((link)=>(
-                <Link href="/" key={link}><span>{link}</span></Link>
-            ))}
+            {about.map((link:string)=>(<FootLink key={link} text={link} />))}
         </div>
         <div className="flex flex-col gap-4">
             <Link href="/"><span className="text-[#FA9706]">Legal</span></Link>
-            {legal.map((link)=>(
-                <Link href="/" key={link}><span>{link}</span></Link>
-            ))}
+            {legal.map((link:string)=>(<FootLink key={link} text={link} />))}
         </div>
         <div className="flex flex-col gap-4">
             <Link href="/"><span className="text-[#FA9706]">Social</span></Link>
-            {social.map((link)=>(
-                <Link href="/" key={link}><span>{link}</span></Link>
-            ))}
+            {social.map((link:string)=>(<FootLink key={link} text={link} />))}
         </div>
         <div className="flex flex-col gap-4">
             <Link href="/"><span className="text-[#FA9706]">Contact</span></Link>
-            {contact.map((link)=>(
-                <Link href="/" key={link}><span>{link}</span></Link>
-            ))}
+            {contact.map((link)=>(<FootLink key={link} text={link} />))}
         </div>
         </div>
         <div className="flex w-full flex-col lg:flex-row gap-8">
