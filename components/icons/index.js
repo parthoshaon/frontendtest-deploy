f085dee6330a5f0b706389d4bc2b996b53fb8053
Udoka <udochukwukaonyela@gmail.com>
2022-08-20 20:57:31 +0100

    fixes

--- components/icons/index.js
+++ components/icons/index.js
@@ -3,7 +3,6 @@ export { default as Star } from "./Star";
 export { default as Stars } from "./Stars";
 export { default as Africa } from "./Africa";
 export { default as Google } from "./Google";
-export { default as LinkedIn } from "./LinkedIn";
 export { default as Facebook } from "./Facebook";
 export { default as Github } from "./Github";
 export { default as Twitter } from "./Twitter";
