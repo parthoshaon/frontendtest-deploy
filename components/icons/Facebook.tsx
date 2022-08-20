9f222def73fd8dd38caca5f21fe7e2b96b6109b7
Udoka <udochukwukaonyela@gmail.com>
2022-08-20 16:10:16 +0100

    added icon prop

--- components/icons/Facebook.tsx
+++ components/icons/Facebook.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
-
-const Facebook: FC<{ className: string }> = (props) => {
+import { IconProps } from "../../typescript"
+const Facebook: FC<IconProps> = (props) => {
   return (
       <svg {...props} version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
           viewBox="0 0 291.319 291.319">
