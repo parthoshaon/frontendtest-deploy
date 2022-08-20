db7fec40602eb68d63a608d7956ca8f7ed6b5901
Udoka <udochukwukaonyela@gmail.com>
2022-08-20 16:37:45 +0100

    added types

--- typescript/index.d.ts
+++ typescript/index.d.ts
@@ -1,8 +1,16 @@
+import { ReactNode } from "react";
 declare global {
     declare namespace Types {
         export interface IconProps {
             className?: string;
         }
+        export interface Props { }
+        export type PropTypes<T, P> = T & P;
+        export type Content = {
+    icon: ReactNode,
+    header: string,
+    text: string
+}
     }
 }
 export = Types;
