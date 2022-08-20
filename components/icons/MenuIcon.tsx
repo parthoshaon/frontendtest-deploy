cf4751f0340aba05e84b919b5b55ed1557386cc4
Udoka <udochukwukaonyela@gmail.com>
2022-08-20 15:46:02 +0100

    added types

--- components/icons/MenuIcon.tsx
+++ components/icons/MenuIcon.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { FC } from 'react';
 
-const MenuIcon = (props) => {
+const MenuIcon:FC<{className?: string}> = (props) => {
   return (
     <div>MenuIcon</div>
   )
