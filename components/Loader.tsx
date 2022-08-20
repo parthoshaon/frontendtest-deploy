80fae5f1ee0400ac039e778f2fa195eca79f0c37
Udoka <udochukwukaonyela@gmail.com>
2022-08-20 21:50:45 +0100

    Created loader component

---
+++ components/Loader.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import type {Props} from "../typescript";
+
+
+const Loader:FC<Props> = (props) => {
+  return (
+      <div className="w-full h-screen">
+      </div>
+    
+  )
+}
+
+export default Loader
