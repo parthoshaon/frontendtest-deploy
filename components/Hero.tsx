a8a47a8db80778e188973ab90af9d3da0292ac90
Udoka <udochukwukaonyela@gmail.com>
2022-08-20 21:51:13 +0100

    styles

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,7 +13,7 @@ const Hero = () => {
       animate={{ y: 0 }}
       transition={{delay: 0, type: "spring", stiffness: 120 }}
        className="flex flex-col items-center content w-full">
-      <SearchBar heading={"First Ever Tech Personality Award For Africans in West Africa"} />
+      <SearchBar  className="w-full"  heading={"First Ever Tech Personality Award For Africans in West Africa"} />
       <Link href="/voting" >
         <span className="font-semibold flex gap-4 mt-8 cursor-pointer text-[#0c0114] group group-hover:ml-4"><span className="my-auto group-hover:ml-6 transition-all duration-500"> View all categories</span>
         <Arrow className="my-auto group-hover:ml-4 transition-all duration-500 w-6 h-6" />
