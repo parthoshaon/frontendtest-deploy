d799a9ccae517720ec8f9f443f32e0fb9c658f23
Udoka <udochukwukaonyela@gmail.com>
2022-08-20 15:48:32 +0100

    exported menuIcons

--- components/icons/index.js
+++ components/icons/index.js
@@ -9,3 +9,4 @@ export { default as Github } from "./Github";
 export { default as Twitter } from "./Twitter";
 export { default as Logo } from "./Logo";
 export { default as Arrow } from "./Arrow";
+export { default as MenuIcon } from "./MenuIcon";
