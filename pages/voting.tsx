779e9618ef4fa704e48ef2d0aa3b6049786bd8c1
Udoka <udochukwukaonyela@gmail.com>
2022-08-20 21:51:41 +0100

    added search bar component

--- pages/voting.tsx
+++ pages/voting.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { Navbar } from '../components';
 import { Arrow, MenuIcon, Search } from "../components/icons";
 import { motion } from "framer-motion";
+import { SearchBar } from "../components/util";
 const data = ["Exceptional Data Analyst", "Promising Data Analyst","Exceptional Data Scientist","Promising Data Scientist","Exceptional Visual Designer","Promising Visual Designer","Exceptional Devops Expert","Exceptional Data Analyst","Exceptional Data Analyst","Promising Data Analyst", "Exceptional Data Scientist","Promising Data Scientist","Exceptional Visual Designer","Promising Visual Designer","Exceptional DevOps Expert","Exceptional Data Analyst"]
 const voting = () => {
   const {span: Span} = motion;
@@ -17,18 +18,7 @@ const voting = () => {
   return (
     <>
     <Navbar  loggedIn={state}/>
-      <div className="flex flex-col items-center content mt-12">
-          <h1 className="text-center lg:text-7xl text-3xl font-medium w-4/5 text-[#0c0114]">Voting Categories</h1>
-          <div className="lg:w-1/2 w-4/5 flex items-center mx-auto mt-8 gap-6">
-            <label className="w-full relative block mx-auto">
-            <span className="absolute inset-y-0 left-2 flex items-center pl-2">
-              <Search />
-                </span>
-                  <input type="text" className="block bg-white w-full py-2 pl-12 pr-3 shadow-sm outline-none border h-[auto] align-middle my-auto rounded-md" placeholder="Nominee's Name, Nominee's Category"/>
-                </label>
-
-                <span className="p-4 text-[#EEE8F2] hover:bg-[#EEE8F2] bg-[#3E0563] hover:text-[#3E0563] transition-all duration-500 ease-in font-semibold rounded cursor-pointer">Search</span>
-          </div>
+      <SearchBar  className="w-full mt-12"  heading={"Voting Categories"}/>
           <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-8 lg:p-12 md:p-8 p-6">
           {data.map((data,i)=>(
             <Link href={`/voting/${parser(data)}`} key={i}>
@@ -47,7 +37,6 @@ const voting = () => {
   </Link>
           ))}
          </div> 
-    </div>
     </>
   )
 }
