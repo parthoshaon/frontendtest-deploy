17ff41996af62d07cb36bdbf07fb5a510017d70d
Udoka <udochukwukaonyela@gmail.com>
2022-08-20 20:50:24 +0100

    added search bar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from 'next/link';
 import { motion } from "framer-motion";
 import { Arrow } from "./icons";
+import { SearchBar } from "./util";
 const Hero = () => {
   const { div: Div } = motion;
   return (
@@ -12,7 +13,7 @@ const Hero = () => {
       animate={{ y: 0 }}
       transition={{delay: 0, type: "spring", stiffness: 120 }}
        className="flex flex-col items-center content w-full">
-      
+      <SearchBar heading={"First Ever Tech Personality Award For Africans in West Africa"} />
       <Link href="/voting" >
         <span className="font-semibold flex gap-4 mt-8 cursor-pointer text-[#0c0114] group group-hover:ml-4"><span className="my-auto group-hover:ml-6 transition-all duration-500"> View all categories</span>
         <Arrow className="my-auto group-hover:ml-4 transition-all duration-500 w-6 h-6" />
