0b101b7eeaa8d76e3a6a9b5b094703a4943dc542
Udoka <udochukwukaonyela@gmail.com>
2022-08-20 17:33:08 +0100

    added type to import

--- pages/voting.tsx
+++ pages/voting.tsx
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from 'react'
 import Link from 'next/link';
 import { Navbar } from '../components';
 import { Arrow, MenuIcon } from "../components/icons";
+import { motion } from "framer-motion";
 const data = ["Exceptional Data Analyst", "Promising Data Analyst","Exceptional Data Scientist","Promising Data Scientist","Exceptional Visual Designer","Promising Visual Designer","Exceptional Devops Expert","Exceptional Data Analyst","Exceptional Data Analyst","Promising Data Analyst", "Exceptional Data Scientist","Promising Data Scientist","Exceptional Visual Designer","Promising Visual Designer","Exceptional DevOps Expert","Exceptional Data Analyst"]
 const voting = () => {
+  const {span: Span} = motion;
   const [state, setState] = useState(false);
   const parser = (arg:string):string =>{
     var string  = arg?.replace(' ','-');
@@ -26,13 +28,20 @@ const voting = () => {
                 <span className="p-4 text-[#EEE8F2] hover:bg-[#EEE8F2] bg-[#3E0563] hover:text-[#3E0563] transition-all duration-500 ease-in font-semibold rounded cursor-pointer">Search</span>
           </div>
           <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-8 lg:p-12 md:p-8 p-6">
-          {data.map((data,index)=>(
-            <Link href={`/voting/${parser(data)}`} key={index}>
-            <span className="flex items-center flex-col gap-6 p-6 shadow-xl hover:shadow-2xl group transition-all duration-500 ease-in cursor-pointer select-none">
+          {data.map((data,i)=>(
+            <Link href={`/voting/${parser(data)}`} key={i}>
+            <Span 
+              initial={{
+        y: ((i % 2 !==0) ? -50 : 50),
+        opacity: 0
+      }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{delay: 0, type: "spring", stiffness: 120 }}
+            className="flex items-center flex-col gap-6 p-6 shadow-xl hover:shadow-2xl group transition-all duration-500 ease-in cursor-pointer select-none">
               <MenuIcon />
               <span>{data}</span>
-<Arrow />
-  </span>
+  <Arrow className="w-4 h-4 float-right ml-auto mr-8 group-hover:mr-4 transition-all duration-500 ease-in cursor-pointer"/>
+  </Span>
   </Link>
           ))}
          </div> 
