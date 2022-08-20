3ed080f6c60fe01bf513b4ba8ebd7a577f8fe023
Udoka <udochukwukaonyela@gmail.com>
2022-08-20 17:33:05 +0100

    added type to import

--- components/icons/Twitter.tsx
+++ components/icons/Twitter.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react'
-import { IconProps } from "../../typescript"
+import type { IconProps } from "../../typescript"
 
 const Twitter:FC<IconProps> = (props) => {
   return (
