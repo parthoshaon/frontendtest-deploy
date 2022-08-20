0de5b295a7ee676675fc2f99e918fb3f70ffa196
Udoka <udochukwukaonyela@gmail.com>
2022-08-20 15:41:50 +0100

    added svg to it

--- components/icons/Arrow.tsx
+++ components/icons/Arrow.tsx
@@ -1,8 +1,12 @@
 import React, { FC } from 'react'
 
-const Arrow = () => {
+const Arrow:FC = () => {
   return (
-    <div>Arrow</div>
+      <svg className="w-4 h-4 float-right ml-auto mr-8 group-hover:mr-4 transition-all duration-500 ease-in cursor-pointer" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M15 18L21 12L15 6" stroke="#0C0114" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+          <path d="M3 12H20" stroke="#0C0114" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+          <path d="M21 12H20" stroke="#0C0114" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+      </svg>
   )
 }
 
