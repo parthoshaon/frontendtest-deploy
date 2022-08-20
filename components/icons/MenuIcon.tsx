4a8bbcaa9ded74e1a1b86dcba0015ce1d7fe6f13
Udoka <udochukwukaonyela@gmail.com>
2022-08-20 16:10:41 +0100

    added icon prop

--- components/icons/MenuIcon.tsx
+++ components/icons/MenuIcon.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
+import { IconProps } from "../../typescript"
 
-const MenuIcon:FC<{className?: string}> = (props) => {
+const MenuIcon:FC<IconProps> = (props) => {
   return (
         <svg {...props} width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
   <rect width="40" height="40" rx="8" fill="#EEE8F2"/>
